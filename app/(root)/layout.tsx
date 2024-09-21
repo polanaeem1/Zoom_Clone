3b1c0e49419a5d8acf1dddef9b29a8c96161729e
polanaeem1 <naeemros9@gmail.com>
2024-09-21 12:54:23 +0300

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React, { Children, ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Zoom Clone",
+  description: "Video Calling App",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
